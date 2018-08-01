bee326c0e303341480993c48cd2de49f7d43a708
Raina Matthews <rainamatthews@gmail.com>
2018-08-01 16:59:24 -0700

    latest updates.  db connections working and some data being written. apparently though I need to look more into these responses cos if there's missing keys it blows up.

---
+++ initial.py
@@ -0,0 +1,66 @@
+import sqlite3
+import datetime
+conn = sqlite3.connect('example2.db') #this will create if it doesn't exist
+c = conn.cursor()
+
+
+
+def checkTableExists(dbcon, tablename):
+    c = dbcon.cursor()
+    try:
+        c.execute("SELECT 1 FROM "+tablename+" LIMIT 1;")
+        exists = True
+        return True
+    except sqlite3.OperationalError as e:
+            message = e.args[0]
+            if message.startswith("no such table"):
+               # print("Table "+tablename+" does not exist")
+                return False
+                #exists = False
+            else:
+                raise
+
+if (checkTableExists(conn,'weatherCurrentconditions')):
+    True
+else:
+    c.execute('''CREATE TABLE weatherCurrentconditions (DateEntered date, time text, summary text, icon text, nearestStormDistance text,precipIntensity text,precipProbability text,temperature text,apparentTemperature text,windSpeed text,windGust text,uvIndex text,visibility text)''')
+    conn.commit()
+    
+if (checkTableExists(conn,'weather')):
+    True
+else:
+    c.execute('''CREATE TABLE weather (DateEntered date, summary text, icon text, sunriseTime text, sunsetTime text,moonPhase text,precipIntensity text, precipIntensityMax text,precipIntensityMaxTime text,precipProbability text,precipType text,temperatureHigh text,temperatureHighTime text,temperatureLow text )''')
+    conn.commit()    
+if (checkTableExists(conn,'AQICurrentConditions')):
+    True
+else:
+    c.execute('''CREATE TABLE AQICurrentConditions (DateEntered date, DateObserved date, HourObserved text, LocalTimeZone text,ReportingArea text,StateCode text,Latitude text,Longitude text,ParameterName text,AQI text,CategoryNumber text, CategoryName text)''')
+    conn.commit()    
+    
+if (checkTableExists(conn,'AQI')):
+    True
+#    c.execute("INSERT INTO AQI VALUES ('"+datetime.datetime.now()+"','BUY','RHATddd',100,35.14)")
+#    conn.commit()
+else:
+    c.execute('''CREATE TABLE AQI (DateEntered date, ForecastDate date, AQI text, Comments text)''')
+    conn.commit()
+    
+if (checkTableExists(conn,'Nagios')):
+    c.execute("INSERT INTO Nagios VALUES ('2006-01-05','BUY','RHATddd',100,35.14)")
+    conn.commit()
+else:
+    c.execute('''CREATE TABLE Nagios (date text, trans text, symbol text, qty real, price real)''')
+    conn.commit()
+    
+#c.execute('''CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+#conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+#conn.close()
+
